a656b94a6a389946d84b1a4f823bfedcf3bcbcdc
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2022-12-29 19:06:27 +0500

    Task Completed

--- prisma/migrations/20221229045118_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `users` MODIFY `name` VARCHAR(255) NULL,
-    MODIFY `otp` INTEGER NULL,
-    MODIFY `otp_expiration_date` INTEGER NULL,
-    MODIFY `phone_number` INTEGER NULL;
--- prisma/migrations/20221229140137_user/migration.sql
+++ prisma/migrations/20221229140137_user/migration.sql
@@ -1,22 +1,13 @@
-/*
-  Warnings:
-
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `users`;
-
 -- CreateTable
 CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NULL,
-    `phone_number` INTEGER NULL,
+    `phone_number` VARCHAR(255) NOT NULL,
     `otp` INTEGER NULL,
     `otp_expiration_date` INTEGER NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `user_name_key`(`name`),
+    UNIQUE INDEX `user_phone_number_key`(`phone_number`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
